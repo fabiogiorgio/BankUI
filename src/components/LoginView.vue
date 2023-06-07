a653333035a5dd1338e5d0506a4f1d55fbf55cf5
Fabio Giorgio <fabiogiorgio@outlook.com>
2023-06-07 18:17:04 +0200

    first commit

---
+++ src/components/LoginView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div v-if="!loggedIn">
+    <form name="login-form">
+      <img src="@/assets/SawaLogo.png" alt="Logo">
+      <div class="mb-3">
+        <label for="userName">Username: </label>
+        <input type="text" id="userName" v-model="input.userName" />
+      </div>
+      <div class="mb-3">
+        <label for="password">Password: </label>
+        <input type="password" id="password" v-model="input.password" />
+      </div>
+      <button class="btn btn-outline-dark" type="submit" v-on:click.prevent="login()">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { SET_AUTHENTICATION, SET_USERNAME, SET_ID } from "../store/storeconstants";
+
+export default {
+  name: 'LoginView',
+  data() {
+    return {
+      input: {
+        userName: "",
+        password: ""
+      },
+      output: "",
+      loggedIn: false
+    }
+  },
+  methods: {
+    login() {
+      //make sure username OR password are not empty
+      if (this.input.userName != "" || this.input.password != "") {
+
+        axios.post('http://localhost:8080/api/login', {
+          userName: this.input.userName ,
+          password: this.input.password
+
+        })
+          .then(response => {
+            const token = response.data.bearerToken;
+            localStorage.setItem('token', token);
+            this.$store.commit(`auth/${SET_AUTHENTICATION}`, true);
+            this.$store.commit(`auth/${SET_USERNAME}`, this.input.userName);
+            this.$store.commit(`auth/${SET_ID}`, response.data);
+            this.loggedIn = true;
+            this.$router.push('/home')
+          })
+          
+          .catch(error => {
+            alert("I love pizza");
+            console.log(error);
+            // handle login error, display message to user, etc
+          });
+        
+      } else {
+        this.$store.commit(`auth/${SET_AUTHENTICATION}`, false);
+        this.output = "Username and password can not be empty"
+        console.log("end of else")
+      }
+    },
+  },
+}
+</script>
---
+++ src/components/Users.vue
@@ -0,0 +1,78 @@
+<template>
+    <welcomeview></welcomeview>
+    <h3>Deactivate the desired user by clicking on the green button</h3>
+    <div>
+        <ul class="user-list">
+            <li v-for="user in users" :key="user.id">
+                {{ user.name }} (ID: {{ user.id }})
+                <button @click="deactivateUser(user.id)">Deactivate</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import WelcomeView from './WelcomeView.vue'
+export default {
+    name: 'Users',
+    components: {
+        welcomeview: WelcomeView
+    },
+    data() {
+        return {
+            users: []
+        }
+    },
+    mounted() {
+        this.getUsers()
+    },
+    methods: {
+        getUsers(){
+            const token = localStorage.getItem('token');
+            axios.get('http://localhost:8080/api/users', {
+             headers: {
+                Authorization: `Bearer ${token}`
+            }
+            })
+            .then(response => {
+                this.users = response.data;
+            });
+        },
+        deactivateUser(id) {
+            axios.put(`https://sawacms.azurewebsites.net/api/users/${id}/false`)
+                .then(() => {
+                    this.getUsers()
+                });
+        }
+    }
+}
+</script>
+
+<style>
+.user-list {
+    list-style: none;
+    padding: 0;
+}
+
+.user-list li {
+    padding: 10px;
+    border-bottom: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.user-list button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 5px 10px;
+    border: none;
+    cursor: pointer;
+}
+
+.user-list button:hover {
+    background-color: #3e8e41;
+}
+</style>
+
